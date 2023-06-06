ab8b7d92c8e01a2bc0752240042c07cf3d6658b5
Rithin Ashikh <rithinashikhev@gmail.com>
2023-06-06 21:11:30 +0530

    serializer changed

--- phase1/serializer.py
+++ phase1/serializer.py
@@ -5,4 +5,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['id','name','department','salary']
+        fields = '__all__'
